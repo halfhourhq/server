1d31c52cfcf70666c89f230724f5a58127748508
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-09-21 23:23:48 +0200

    Rewrites IP handing

--- misc/rate_limiter.util.ts
+++ misc/rate_limiter.util.ts
@@ -8,16 +8,16 @@ export async function rate_limiter(c: Context, next: Next) {
     requests: number
     expires_at: number
   }
-  const info = getConnInfo(c)
-  const ip = (await kv.get<Access>(['requests', info.remote.address])).value
+  const address = c.req.header('X-Forwarded-For') || getConnInfo(c).remote.address
+  const ip = (await kv.get<Access>(['requests', address])).value
   if(!ip){
-    await kv.set(['requests', info.remote.address], { requests: 1, expires_at: Date.now()+(1000*60) }, { expireIn: 1000*60 })
+    await kv.set(['requests', address], { requests: 1, expires_at: Date.now()+(1000*60) }, { expireIn: 1000*60 })
   } else if(ip.expires_at < Date.now()){
-    await kv.set(['requests', info.remote.address], { requests: 1, expires_at: Date.now()+(1000*60) }, { expireIn: 1000*60 })
+    await kv.set(['requests', address], { requests: 1, expires_at: Date.now()+(1000*60) }, { expireIn: 1000*60 })
   } else if(ip.expires_at >= Date.now() && ip.requests >= 15){
     throw new HTTPException(429, { message: 'You have reached the maximum 10 requests per minute' })
   } else if(ip.expires_at >= Date.now() && ip.requests < 15){
-    await kv.set(['requests', info.remote.address], { requests: ++ip.requests, expires_at: ip.expires_at })
+    await kv.set(['requests', address], { requests: ++ip.requests, expires_at: ip.expires_at })
   }
   await next()
 }
